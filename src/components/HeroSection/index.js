83f49bbbc7c2df6f756d6adf06436df215fc3a5d
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2021-04-23 20:19:59 +0530

    fix

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { HeroContainer, HeroRow, HeroTextWrapper, ButtonWrapper, Title, Highlight, SocialMediaIconWrapper, SocialButton, Icon, FaI, FaT, FaG, FaL, Span, Resume, Arrow, Button } from './HeroSection.styles.jsx';
-import './Animation.jsx';
+import { Stars, Stars2, Stars3 } from './Animation';
 import pdf from '../../assests/Resume.pdf';
+// import '../../assests/styles.css';
 
 function HeroSection()
 {   
     return (
-        <>
+        <>  
             <HeroContainer>
+                <Stars />
+                <Stars2 />
+                <Stars3 />
                 <HeroRow>
                     <HeroTextWrapper>
                         <Title>Hello, I'm <Highlight>Harish Lal</Highlight>.</Title>
