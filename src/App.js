2e74f07cddf1dcd6c677af1f621b5692311db93e
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2021-05-11 15:14:21 +0530

    homepage changed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path='/Portfolio-Website/' component={Home} />
+        <Route exact path='/' component={Home} />
       </Switch>
     </Router>
   );
