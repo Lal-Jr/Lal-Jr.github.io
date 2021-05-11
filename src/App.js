f453e70d62ca26d25bf93232ced311cc7af33a83
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2021-05-11 15:57:37 +0530

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path='/' component={Home} />
+        <Route exact path='/Portfolio' component={Home} />
       </Switch>
     </Router>
   );
