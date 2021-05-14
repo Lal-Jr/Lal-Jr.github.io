790646e41ebf4477049e91eb77b9e7a7fb1e8a54
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2021-05-14 15:56:58 +0530

    Social Media Icons Updated

--- src/components/HeroSection/HeroSection.styles.jsx
+++ src/components/HeroSection/HeroSection.styles.jsx
@@ -1,233 +1,287 @@
 import styled from "styled-components";
-import { FaInstagram, FaTwitter, FaLinkedin, FaGithub } from "react-icons/fa";
 import { Link as LinkR } from "react-router-dom";
 import { Link as LinkS } from "react-scroll";
 import { AiOutlineArrowRight } from "react-icons/ai";
 
 export const HeroContainer = styled.div`
-  height: 100vh;
-  background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);
-  overflow: hidden;
+	height: 100vh;
+	background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);
+	overflow: hidden;
 `;
 
 export const HeroRow = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100%;
 `;
 
 export const HeroTextWrapper = styled.div`
-  @media screen and(max - width: 768px) {
-    padding-bottom: 65px;
-  }
+	@media screen and(max - width: 768px) {
+		padding-bottom: 65px;
+	}
 `;
 
 export const Title = styled.h1`
-  color: #fff;
-  font-size: 48px;
-  font-weight: 900px;
-  letter-spacing: 1.5px;
-  padding: 5px 20px 5px 20px;
-  align-text: center;
-  z-index: 1;
-  display: flex;
-  justify-content: center;
+	color: #fff;
+	font-size: 48px;
+	font-weight: 900px;
+	letter-spacing: 1.5px;
+	padding: 5px 20px 5px 20px;
+	align-text: center;
+	z-index: 1;
+	display: flex;
+	justify-content: center;
 
-  @media (max-width: 864px) {
-    font-size: 36px;
-  }
+	@media (max-width: 864px) {
+		font-size: 36px;
+	}
 
-  @media (max-width: 768px) {
-    font-size: 26px;
-  }
+	@media (max-width: 768px) {
+		font-size: 26px;
+	}
 
-  @media (max-width: 555px) {
-    font-size: 18px;
-  }
+	@media (max-width: 555px) {
+		font-size: 18px;
+	}
 
-  @media (max-width: 388px) {
-    font-size: 16px;
-  }
+	@media (max-width: 388px) {
+		font-size: 16px;
+	}
 
-  @media (max-width: 355px) {
-    font-size: 12px;
-  }
+	@media (max-width: 355px) {
+		font-size: 12px;
+	}
 
-  @media (max-width: 290px) {
-    font-size: 10px;
-  }
+	@media (max-width: 290px) {
+		font-size: 10px;
+	}
 `;
 
 export const ButtonWrapper = styled.div`
-  display: flex;
-  justify-content: center;
+	display: flex;
+	justify-content: center;
 `;
 
 export const Highlight = styled(LinkS)`
-  -webkit-text-fill-color: transparent;
-  -webkit-text-stroke-width: 1.5px;
-  display: inline-block;
-  padding: 0px 10px;
+	-webkit-text-fill-color: transparent;
+	-webkit-text-stroke-width: 1.5px;
+	display: inline-block;
+	padding: 0px 10px;
 
-  &:hover {
-    -webkit-text-fill-color: #46caca;
-    -webkit-text-stroke-width: 0px;
-    cursor: pointer;
-  }
+	&:hover {
+		-webkit-text-fill-color: #46caca;
+		-webkit-text-stroke-width: 0px;
+		cursor: pointer;
+	}
 
-  @media (max-width: 864px) {
-    -webkit-text-stroke-width: 1.3px;
-  }
+	@media (max-width: 864px) {
+		-webkit-text-stroke-width: 1.3px;
+	}
 
-  @media (max-width: 768px) {
-    -webkit-text-stroke-width: 1px;
-  }
+	@media (max-width: 768px) {
+		-webkit-text-stroke-width: 1px;
+	}
 
-  @media (max-width: 555px) {
-    -webkit-text-stroke-width: 0.8px;
-  }
+	@media (max-width: 555px) {
+		-webkit-text-stroke-width: 0.8px;
+	}
 
-  @media (max-width: 388px) {
-    -webkit-text-stroke-width: 0.6px;
-  }
+	@media (max-width: 388px) {
+		-webkit-text-stroke-width: 0.6px;
+	}
 
-  @media (max-width: 355px) {
-    -webkit-text-stroke-width: 0.4px;
-  }
+	@media (max-width: 355px) {
+		-webkit-text-stroke-width: 0.4px;
+	}
 
-  @media (max-width: 290px) {
-    -webkit-text-stroke-width: 0.2px;
-  }
+	@media (max-width: 290px) {
+		-webkit-text-stroke-width: 0.2px;
+	}
 `;
 
-export const SocialMediaIconWrapper = styled.div`
-  margin: 0;
-  padding: 0;
-  box-sozing: border-box;
-  font-family: "Poppins", sans-serif;
-  position: absolute;
-  right: 2rem;
-  bottom: 2rem;
-
-  @media (max-width: 560px) {
-    bottom: 0.5rem;
-  }
-`;
-
-export const Icon = styled.div`
-  display: inline-flex;
-  height: 60px;
-  width: 60px;
-  border-radius: 50px;
-  box-sizing: border-box;
-  line-height: 60px;
-  justify-content: center;
-  align-items: center;
-
-  @media (max-width: 320px) {
-    width: 30px;
-    height: 30px;
-  }
-`;
-
-export const SocialButton = styled.div`
-  display: inline-block;
-  height: 60px;
-  width: 60px;
-  float: left;
-  margin: 5px;
-  background: #fff;
-  border-radius: 50px;
-  cursor: pointer;
-  box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.1);
-  transition: all 0.3s ease-out;
-  overflow: hidden;
-
-  &:hover {
-    width: 200px;
-    ${Icon} {
-      color: #fff;
-      background: #1b2735;
-      transition: all 0.2s ease-in-out;
-    }
-
-    @media (max-width: 768px) {
-      width: 60px;
-    }
-  }
-
-  @media (max-width: 320px) {
-    width: 30px;
-    height: 30px;
-  }
-`;
-
-export const FaI = styled(FaInstagram)`
-  font-size: 25px;
-  line-height: 60px;
-`;
-export const FaT = styled(FaTwitter)`
-  font-size: 25px;
-  line-height: 60px;
-`;
-export const FaG = styled(FaGithub)`
-  font-size: 25px;
-  line-height: 60px;
-`;
-export const FaL = styled(FaLinkedin)`
-  font-size: 25px;
-  line-height: 60px;
+export const Icon = styled.a`
+	width: 50px;
+	height: 50px;
+	display: block;
+	transition: 0.5s;
+	background: #ccc;
 `;
 
 export const Span = styled.span`
-  display: inline-flex;
-  font-size: 20px;
-  font-weight: bold;
-  margin-left: 10px;
-  padding-top: -10px;
-  justify-content: center;
-  text-align: center;
+	font-weight: bold;
+	width: 100%;
+	height: 100%;
+
+	&:before {
+		font-family: fontAwesome;
+		text-align: center;
+		line-height: 50px;
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background: #fff;
+		color: #262626;
+		font-size: 30px;
+		transform-origin: top;
+		transition: transform 0.5s;
+	}
+
+	&:after {
+		font-family: fontAwesome;
+		text-align: center;
+		line-height: 50px;
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background: #fff;
+		color: #262626;
+		font-size: 30px;
+		transform-origin: bottom;
+		transition: transform 0.5s;
+		transform: rotateX(90deg) translateY(50%);
+	}
+`;
+
+export const SocialMediaIconWrapper = styled.ul`
+	position: absolute;
+	right: 2rem;
+	bottom: 2rem;
+	margin: 0;
+	padding: 0;
+	display: flex;
+`;
+
+export const SocialButton = styled.li`
+	list-style: none;
+	position: relative;
+	margin: 0 10px;
+
+	&:before {
+		content: "";
+		position: absolute;
+		left: 0;
+		bottom: -8px;
+		width: 50px;
+		height: 10px;
+		background: #fff;
+		border-radius: 50%;
+		transition: 0.5s;
+		opacity: 0;
+		filter: blur(2px);
+		transform: scale(0.8);
+	}
+
+	&:hover {
+		&:before {
+			transition-delay: 0.2s;
+			opacity: 0.2;
+			transform: scale(1);
+		}
+		${Icon} {
+			transform: translateY(-10px);
+		}
+		${Span} {
+			&:before {
+				transform: rotateX(90deg) translateY(-50%);
+			}
+			&:after {
+				transform: rotateX(0deg) translateY(0);
+			}
+		}
+	}
+	&:nth-child(1) {
+		${Span} {
+			&:before {
+				content: "\f16d";
+			}
+			&:after {
+				background: #c13584;
+				color: #fff;
+				content: "\f16d";
+			}
+		}
+	}
+	&:nth-child(2) {
+		${Span} {
+			&:before {
+				content: "\f0e1";
+			}
+			&:after {
+				background: #0077b5;
+				color: #fff;
+				content: "\f0e1";
+			}
+		}
+	}
+	&:nth-child(3) {
+		${Span} {
+			&:before {
+				content: "\f09b";
+			}
+			&:after {
+				background: #212529;
+				color: #fff;
+				content: "\f09b";
+			}
+		}
+	}
+	&:nth-child(4) {
+		${Span} {
+			&:before {
+				content: "\f099";
+			}
+			&:after {
+				background: #55acee;
+				color: #fff;
+				content: "\f099";
+			}
+		}
+	}
 `;
 
 export const Arrow = styled(AiOutlineArrowRight)`
-  margin-left: 5px;
+	margin-left: 5px;
 `;
 
 export const Resume = styled(LinkR)`
-  position: absolute;
-  right: 2rem;
-  top: 2rem;
-  background: transparent;
-  white-space: nowrap;
-  padding: 10px 20px;
-  color: #fff;
-  font-size: 18px;
-  letter-spacing: 1px;
-  text-decoration: none;
-  border: 2px solid #fff;
-  cursor: pointer;
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  transition: all 0.2s ease-in-out;
-  font-weight: bold;
+	position: absolute;
+	right: 2rem;
+	top: 2rem;
+	background: transparent;
+	white-space: nowrap;
+	padding: 10px 20px;
+	color: #fff;
+	font-size: 18px;
+	letter-spacing: 1px;
+	text-decoration: none;
+	border: 2px solid #fff;
+	cursor: pointer;
+	display: inline-flex;
+	justify-content: center;
+	align-items: center;
+	transition: all 0.2s ease-in-out;
+	font-weight: bold;
 
-  &:hover {
-    transition: all 0.2s ease-in-out;
-    background: #46caca;
-    color: #fff;
-    border: 2px solid #46caca;
-    ${Arrow} {
-      transform: translateX(5px);
-      transition: all 0.3s ease-in-out;
-    }
-  }
+	&:hover {
+		transition: all 0.2s ease-in-out;
+		background: #46caca;
+		color: #fff;
+		border: 2px solid #46caca;
+		${Arrow} {
+			transform: translateX(5px);
+			transition: all 0.3s ease-in-out;
+		}
+	}
 
-  @media (max-width: 560px) {
-    padding: 3px 8px;
-    font-size: 12px;
-    right: 1rem;
-    top: 1rem;
-  }
+	@media (max-width: 560px) {
+		padding: 3px 8px;
+		font-size: 12px;
+		right: 1rem;
+		top: 1rem;
+	}
 `;
--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,102 +1,106 @@
 import React from "react";
 import {
-  HeroContainer,
-  HeroRow,
-  HeroTextWrapper,
-  ButtonWrapper,
-  Title,
-  Highlight,
-  SocialMediaIconWrapper,
-  SocialButton,
-  Icon,
-  FaI,
-  FaT,
-  FaG,
-  FaL,
-  Span,
-  Resume,
-  Arrow,
+	HeroContainer,
+	HeroRow,
+	HeroTextWrapper,
+	ButtonWrapper,
+	Title,
+	Highlight,
+	SocialMediaIconWrapper,
+	SocialButton,
+	Icon,
+	Span,
+	Resume,
+	Arrow,
 } from "./HeroSection.styles.jsx";
 import { Stars, Stars2, Stars3 } from "../../assests/Animation.jsx";
 import pdf from "../../assests/Resume.pdf";
 
 function HeroSection() {
-  return (
-    <>
-      <HeroContainer id="home">
-        <Stars />
-        <Stars2 />
-        <Stars3 />
-        <HeroRow>
-          <HeroTextWrapper>
-            <Title>
-              Hello, I'm
-              <Highlight
-                activeClass="active"
-                to="about"
-                spy={true}
-                smooth={true}
-                offset={0}
-                duration={800}
-              >
-                Harish Lal
-              </Highlight>
-              .
-            </Title>
-            <Title>
-              I'm a
-              <Highlight
-                activeClass="active"
-                to="work"
-                spy={true}
-                smooth={true}
-                offset={0}
-                duration={800}
-              >
-                Front-End
-              </Highlight>
-              Web Developer.
-            </Title>
-            <ButtonWrapper>
-              <Resume to={pdf} target="_blank">
-                My Resume
-                <Arrow />
-              </Resume>
-            </ButtonWrapper>
-            <SocialMediaIconWrapper>
-              <SocialButton>
-                <Span>
-                  <Icon>
-                    <FaI />
-                  </Icon>
-                </Span>
-              </SocialButton>
-              <SocialButton>
-                <Span>
-                  <Icon>
-                    <FaL />
-                  </Icon>
-                </Span>
-              </SocialButton>
-              <SocialButton>
-                <Span>
-                  <Icon>
-                    <FaT />
-                  </Icon>
-                </Span>
-              </SocialButton>
-              <SocialButton>
-                <Span>
-                  <Icon>
-                    <FaG />
-                  </Icon>
-                </Span>
-              </SocialButton>
-            </SocialMediaIconWrapper>
-          </HeroTextWrapper>
-        </HeroRow>
-      </HeroContainer>
-    </>
-  );
+	return (
+		<>
+			<HeroContainer id="home">
+				<Stars />
+				<Stars2 />
+				<Stars3 />
+				<HeroRow>
+					<HeroTextWrapper>
+						<Title>
+							Hello, I'm
+							<Highlight
+								activeClass="active"
+								to="about"
+								spy={true}
+								smooth={true}
+								offset={0}
+								duration={800}
+							>
+								Harish Lal
+							</Highlight>
+							.
+						</Title>
+						<Title>
+							I'm a
+							<Highlight
+								activeClass="active"
+								to="work"
+								spy={true}
+								smooth={true}
+								offset={0}
+								duration={800}
+							>
+								Front-End
+							</Highlight>
+							Web Developer.
+						</Title>
+						<ButtonWrapper>
+							<Resume to={pdf} target="_blank" rel="noreferrer">
+								My Resume
+								<Arrow />
+							</Resume>
+						</ButtonWrapper>
+						<SocialMediaIconWrapper>
+							<SocialButton>
+								<Icon
+									href="https://www.instagram.com/_lal._.jr_/"
+									target="_blank"
+									rel="noreferrer"
+								>
+									<Span></Span>
+								</Icon>
+							</SocialButton>
+							<SocialButton>
+								<Icon
+									href="https://www.linkedin.com/in/laljr-harish/"
+									target="_blank"
+									rel="noreferrer"
+								>
+									<Span></Span>
+								</Icon>
+							</SocialButton>
+							<SocialButton>
+								<Icon
+									href="https://github.com/Lal-Jr"
+									target="_blank"
+									rel="noreferrer"
+								>
+									<Span></Span>
+								</Icon>
+							</SocialButton>
+							<SocialButton>
+								<Icon
+									href="https://twitter.com/LalJr_Harish"
+									target="_blank"
+									rel="noreferrer"
+								>
+									<Span></Span>
+								</Icon>
+							</SocialButton>
+						</SocialMediaIconWrapper>
+					</HeroTextWrapper>
+				</HeroRow>
+			</HeroContainer>
+		</>
+	);
 }
 export default HeroSection;
