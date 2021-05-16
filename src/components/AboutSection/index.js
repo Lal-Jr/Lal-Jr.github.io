7f61905bed3327a22b43a13134e0b4ee1e48c36e
Harish Lal <harishlal80@gmail.com>
2021-05-16 13:49:59 +0530

    text correction

--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -3,7 +3,7 @@ import {
   AboutContainer,
   Title,
   SubText,
-  Icon,
+  //   Icon,
 } from "./AboutSection.styles.jsx";
 
 function AboutSection(icon) {
@@ -12,22 +12,20 @@ function AboutSection(icon) {
       <AboutContainer id="about">
         <Title>About</Title>
         <SubText>
-          Hey, I'm Harish Lal and I'm Web Developer who loves to build things. I
-          am a <strong>Full-time student </strong>pursuing B.Tech in Electrical
-          and Electronics Engineering (EEE) from Government Engineering
-          College,Barton Hill. My Love for Web Development started during
-          Pandemic and I always give 100% when I start to like anything so I
-          during 2020 I started learning HTML, CSS and JS. By the end of 2020,
-          learned enough so as to be said as a Frontend Developer and started
-          doing projects. Starting of 2021, gave me few oppurtunities to work as
-          a freelance developer and improve my skills and get an idea of what I
-          can achieve if I work hard. Long story short I am budding developer
-          who is trying to evolve as time goes by. These as few of the skills
-          I'm good at.
+          Hey, I'm<big> Harish Lal</big> and I'm <big>Web Developer</big> who
+          loves to build things. I am a <big>Full-time student </big>pursuing
+          B.Tech in Electrical and Electronics Engineering (EEE) from Government
+          Engineering College,Barton Hill. My Love for Web Development started
+          during Pandemic and I always give 100% when I start to like anything
+          so I during 2020 I started learning HTML, CSS and JS. By the end of
+          2020, learned enough so as to be said as a Frontend Developer and
+          started doing projects. Starting of 2021, gave me few oppurtunities to
+          work as a freelance developer and improve my skills and get an idea of
+          what I can achieve if I work hard. Long story short I am budding
+          developer who is trying to evolve as time goes by. These as few of the
+          skills I'm good at.
         </SubText>
-        <SubText>
-          <Icon src={icon} />
-        </SubText>
+        <SubText>{/* <Icon src={icon} /> */}</SubText>
       </AboutContainer>
     </>
   );
