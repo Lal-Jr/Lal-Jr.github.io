f45455b3e2c8c9a200ec5aa9f1889bb8595d732f
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2021-06-16 17:05:59 +0530

    gh-pages removed from .json files

--- src/components/Navbar/Navbar.components.jsx
+++ src/components/Navbar/Navbar.components.jsx
@@ -2,79 +2,79 @@ import React, { useState, useEffect } from "react";
 import { FaBars } from "react-icons/fa";
 import { animateScroll as scroll } from "react-scroll";
 import {
-  NavItem,
-  NavLogo,
-  NavMenu,
-  NavBar,
-  Hamburger,
+	NavItem,
+	NavLogo,
+	NavMenu,
+	NavBar,
+	Hamburger,
 } from "./Navbar.styles.jsx";
 
 function Navbar({ toggle }) {
-  const [, setClick] = useState(false);
+	const [, setClick] = useState(false);
 
-  const closeMenu = () => setClick(false);
+	const closeMenu = () => setClick(false);
 
-  const [scrollNav, setScrollNav] = useState(false);
+	const [scrollNav, setScrollNav] = useState(false);
 
-  const changeNav = () => {
-    if (window.scrollY >= window.innerHeight) {
-      setScrollNav(true);
-    } else {
-      setScrollNav(false);
-    }
-  };
+	const changeNav = () => {
+		if (window.scrollY >= window.innerHeight) {
+			setScrollNav(true);
+		} else {
+			setScrollNav(false);
+		}
+	};
 
-  useEffect(() => {
-    window.addEventListener("scroll", changeNav);
-  }, []);
+	useEffect(() => {
+		window.addEventListener("scroll", changeNav);
+	}, []);
 
-  const toggleHome = () => {
-    scroll.scrollToTop();
-  };
+	const toggleHome = () => {
+		scroll.scrollToTop();
+	};
 
-  return (
-    <>
-      <NavBar scrollNav={scrollNav}>
-        <NavLogo onClick={toggleHome}> LALJR </NavLogo>
-        <NavMenu>
-          <NavItem onClick={toggleHome}>Home</NavItem>
-          <NavItem
-            to="about"
-            spy={true}
-            smooth={true}
-            offset={0}
-            duration={800}
-            onClick={closeMenu}
-          >
-            About
-          </NavItem>
-          <NavItem
-            to="work"
-            spy={true}
-            smooth={true}
-            offset={0}
-            duration={800}
-            onClick={closeMenu}
-          >
-            Works
-          </NavItem>
-          <NavItem
-            to="contact"
-            spy={true}
-            smooth={true}
-            offset={0}
-            duration={800}
-            onClick={closeMenu}
-          >
-            Contact Me
-          </NavItem>
-        </NavMenu>
-        <Hamburger onClick={toggle}>
-          <FaBars />
-        </Hamburger>
-      </NavBar>
-    </>
-  );
+	return (
+		<>
+			<NavBar scrollNav={scrollNav}>
+				<NavLogo onClick={toggleHome}> LALJR </NavLogo>
+				<NavMenu>
+					<NavItem onClick={toggleHome}>Home</NavItem>
+					<NavItem
+						to="about"
+						spy={true}
+						smooth={true}
+						offset={0}
+						duration={800}
+						onClick={closeMenu}
+					>
+						About
+					</NavItem>
+					<NavItem
+						to="work"
+						spy={true}
+						smooth={true}
+						offset={0}
+						duration={800}
+						onClick={closeMenu}
+					>
+						Works
+					</NavItem>
+					<NavItem
+						to="contact"
+						spy={true}
+						smooth={true}
+						offset={0}
+						duration={800}
+						onClick={closeMenu}
+					>
+						Contact Me
+					</NavItem>
+				</NavMenu>
+				<Hamburger onClick={toggle}>
+					<FaBars />
+				</Hamburger>
+			</NavBar>
+		</>
+	);
 }
 
 export default Navbar;
--- src/components/Navbar/Navbar.styles.jsx
+++ src/components/Navbar/Navbar.styles.jsx
@@ -2,75 +2,75 @@ import { Link } from "react-scroll";
 import styled from "styled-components";
 
 export const NavBar = styled.div`
-  background: rgba(30, 30, 30, 0.25);
-  backdrop-filter: blur(20px);
-  -webkit-backdrop-filter: blur(20px);
-  height: 80px;
-  visibility: ${({ scrollNav }) => (scrollNav ? "visible" : "hidden")};
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  position: fixed;
-  width: 100vw;
-  transition: all 0.1s ease-in-out;
-  border-bottom: solid 0.25vh #46caca;
-  z-index: 998;
+	background: rgba(30, 30, 30, 0.25);
+	backdrop-filter: blur(20px);
+	-webkit-backdrop-filter: blur(20px);
+	height: 80px;
+	visibility: ${({ scrollNav }) => (scrollNav ? "visible" : "hidden")};
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	position: fixed;
+	width: 100vw;
+	transition: all 0.1s ease-in-out;
+	border-bottom: solid 0.25vh #46caca;
+	z-index: 998;
 
-  @media (max-width: 425px) {
-    height: 60px;
-  }
+	@media (max-width: 425px) {
+		height: 60px;
+	}
 `;
 
 export const Hamburger = styled.div`
-  display: none;
+	display: none;
 
-  @media (max-width: 768px) {
-    display: block;
-    position: fixed;
-    top: 0;
-    right: 0;
-    transform: translate(-100%, 45%);
-    font-size: 1.5rem;
-    cursor: pointer;
-    color: #fff;
-  }
+	@media (max-width: 768px) {
+		display: block;
+		position: fixed;
+		top: 0;
+		right: 0;
+		transform: translate(-100%, 45%);
+		font-size: 1.5rem;
+		cursor: pointer;
+		color: #fff;
+	}
 `;
 
 export const NavLogo = styled.h1`
-  display: block;
-  color: #fff;
-  cursor: pointer;
-  margin-left: 4rem;
-  letter-spacing: 4px;
+	display: block;
+	color: #fff;
+	cursor: pointer;
+	margin-left: 4rem;
+	letter-spacing: 4px;
 
-  @media (max-width: 768px) {
-    margin: 0;
-    transform: translate(20%, 20%);
-    font-size: 1.5rem;
-  }
+	@media (max-width: 768px) {
+		margin: 0;
+		transform: translate(20%, 20%);
+		font-size: 1.5rem;
+	}
 `;
 
 export const NavMenu = styled.div`
-  display: flex;
-  margin-right: 4rem;
+	display: flex;
+	margin-right: 4rem;
 
-  @media (max-width: 768px) {
-    display: none;
-  }
+	@media (max-width: 768px) {
+		display: none;
+	}
 `;
 
 export const NavItem = styled(Link)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 16px;
-  font-weight: bold;
-  text-decoration: none;
-  cursor: pointer;
-  color: #fff;
-  padding-inline: 15px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	font-size: 16px;
+	font-weight: bold;
+	text-decoration: none;
+	cursor: pointer;
+	color: #fff;
+	padding-inline: 15px;
 
-  &:hover {
-    color: #46caca;
-  }
+	&:hover {
+		color: #46caca;
+	}
 `;
